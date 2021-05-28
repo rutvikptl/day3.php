64f5d5c8c3b17eee071d201bd0705c6401c14022
rutvikptl <84767988+rutvikptl@users.noreply.github.com>
2021-05-28 17:42:13 +0530

    registration.php

---
+++ task3.php
@@ -0,0 +1,15 @@
+<?php
+
+$a=$_POST['txt1'];
+$b=$_POST['txt2'];
+$c=$_POST['txt3'];
+$d=$_POST['txt4'];
+$e=$_POST['txt5'];
+
+
+echo " my name is a $a and my age is $b" ;
+echo "my email-id is: $c";
+echo "phone no is= $d";
+echo "address:$e";
+
+?>
