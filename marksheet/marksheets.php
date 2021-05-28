fc6f0867d4a06f7214872dbdfced3e2856583560
rutvikptl <84767988+rutvikptl@users.noreply.github.com>
2021-05-28 17:53:56 +0530

    Add files via upload

---
+++ marksheet/marksheets.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>marksheet</title>
+</head>
+<body>
+	<h1>Marksheet</h1>
+	<form action="total.php" method="post">
+		<table>
+		<tr><td>Gujarti :</td>
+			<td><input type="number" name="txt1"></td>
+		</tr>
+		<tr><td>English :</td>
+			<td><input type="number" name="txt2"></td>
+		</tr>
+		<tr><td>Social science :</td>
+			<td><input type="number" name="txt3"></td>
+		</tr>
+		<tr><td>Science :</td>
+			<td><input type="number" name="txt4"></td>
+		</tr>
+		<tr><td>Hindi :</td>
+			<td><input type="number" name="txt5"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="txt5"></td>
+		</tr>
+	</table>
+	</form>
+
+</body>
+</html>
